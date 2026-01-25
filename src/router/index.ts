99ed45b89530914726038537d0d6b3f3b5bcbf32
Lingard Nguyen <lingard.nguyen@3i.ai>
2026-01-25 15:17:12 +0700

    Remove unused import of Home component from router index file

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
-import Home from "../components/Home.vue"
 import Survey from "../components/Survey.vue"
 import Creator from "../components/Creator.vue"
 import PDFGenerator from "../components/PDFGenerator.vue"
